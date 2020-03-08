d3c49960638d481debaa4e24dee06b2ae092de2d
VictorSad26 <victor.sad@hotmail.com>
2020-03-08 13:34:14 -0300

    Criando os Gets

--- Aluno.java
+++ Aluno.java
@@ -2,4 +2,10 @@ public class Aluno{
     private String nome;
     private int id;
 
+    	public String getNome() {
+    		return this.nome;
+    	}
+    	public int getId() {
+    		return this.id;
+    	}
 }
