7fbf68ff7726855274b1d08ca86d3d0ffd5b8fe0
Guilherme Rocha Ramos <guilhermerxcha@gmail.com>
2020-03-08 20:33:25 -0300

    Update Aluno.java
    master commit

--- Aluno.java
+++ Aluno.java
@@ -1,15 +1,23 @@
 public class Aluno{
     private String nome;
     private int id;
+    
+    public void Aluno(String nome, int id) { // Este � o metodo construtor
+    	this.nome = "Victor";
+    	this.id = "1";
+	this.nome = "Guilherme";
+	this.id = "2";
+    	
+    }
 
-    	public String getNome() {
+    	public String getNome() { // metodo Get
     		return this.nome;
     	}
     	public int getId() {
     		return this.id;
     	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) { // metodo Set
 		this.nome = nome;
 	}
 
