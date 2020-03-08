e3e9cba0f8772968bb7a0cceb0e4f966e12d9e82
VictorSad26 <victor.sad@hotmail.com>
2020-03-08 20:03:27 -0300

    Metodo construtor

--- Aluno.java
+++ Aluno.java
@@ -1,15 +1,21 @@
 public class Aluno{
     private String nome;
     private int id;
+    
+    public void Aluno(String nome, int id) { // Este é o metodo construtor
+    	this.nome = "Victor";
+    	this.id = "1";
+    	
+    }
 
-    	public String getNome() {
+    	public String getNome() { // metodo Get
     		return this.nome;
     	}
     	public int getId() {
     		return this.id;
     	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) { // metodo Set
 		this.nome = nome;
 	}
 
